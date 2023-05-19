555f47a3cf94f50219ab970472648070967703e9
Seba Cardoso <95690554+sebacc92@users.noreply.github.com>
2023-05-19 18:27:47 -0300

    mi primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { FaWhatsapp } from 'react-icons/fa';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function MyWebsite() {
+    return (
+        <div style={{display: 'flex', flexDirection: 'column', height: '100vh', width: '100%'}}>
+            <iframe 
+                src="https://ar.natura.digital-catalogue.com/ar/2023/08/revista/ciclo-08/view/index.html?id_consultora=&fbclid=IwAR2zircPJ2rFEhArnbOeGHX0-PMn3BuwKJQQQMCeJhoMc9Ag535IvuoOxOQ&page=1" 
+                style={{flexGrow: 1, width: '100%', border: '0'}}
+                title="Natura Catalogue"
+            />
+            <div style={{padding: '1em', backgroundColor: 'rgba(255, 255, 255, 0.8)'}}>
+                <h1>Bienvenido/a a nuestro catálogo de productos Natura</h1>
+                <p>Te invitamos a descubrir y comprar nuestros productos en Miramar, Buenos Aires.</p>
+                <p>Contacta a Daiana Lentz a través de WhatsApp al: <a href="https://wa.me/542213096805"><FaWhatsapp color="#25D366" size="1.5em" /> 2213 09-6805</a></p>
+            </div>
+        </div>
+    );
 }
 
-export default App;
+export default MyWebsite;
